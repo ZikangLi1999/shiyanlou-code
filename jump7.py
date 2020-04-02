0fa234bf8b836a32061d2f9f34e81f5ec7ffce06
LZK <zikang_li-2018@sjtu.edu.cn>
2020-04-02 17:32:40 +0800

    shiyanlou-jump7.py

---
+++ jump7.py
@@ -0,0 +1,8 @@
+Code/                                                                                               0000775 0011610 0011610 00000000000 13641320346 012534  5                                                                                                    ustar   shiyanlou                       shiyanlou                                                                                                                                                                                                              Code/jump7.py                                                                                       0000664 0011610 0011610 00000000145 13641320346 014150  0                                                                                                    ustar   shiyanlou                       shiyanlou                                                                                                                                                                                                              for i in range(1, 101):
+	if i % 7 == 0:
+		continue
+	elif '7' in str(i):
+		continue
+	else:
+		print(i)
+                          
